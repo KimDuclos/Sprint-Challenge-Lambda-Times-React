f0f15ee57ee3ba4d499ffbd838720b6f9e0024f4
Kim Duclos <kimduclos@gmail.com>
2019-02-22 09:50:19 -0700

    tab instructions built, with errors

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -6,17 +6,25 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={`tab ${selectedTab === tab ? 'tab active-tab' : null}`}
+      onClick={() => 
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
+         selectedTabHandler(tab)}
+     >
       {props.tab.toUpperCase()}
     </div>
   );
 };
 
 // Make sure you include PropTypes on your props.
+
+
+Tab.propTypes = {
+  tab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  selectTab: PropTypes.string,
+}
+
 
 export default Tab;
