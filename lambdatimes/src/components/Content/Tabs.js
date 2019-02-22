1d7c0d90cfce96f7ff153318f1c50ed66785d8e2
Kim Duclos <kimduclos@gmail.com>
2019-02-22 09:41:48 -0700

    Tabs instructions built, with errors

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,10 +7,19 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {tabs.map((tabs, i) => 
+              <Tab tab={tab} selectTabHandler={selectTabHandler} selectedTab={selectedTab} key={i}/>
+            )}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.prototype = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string
+};
+
 export default Tabs;
